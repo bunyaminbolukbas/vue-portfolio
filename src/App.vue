2c95e6508374c213e701391d01aba6e87fb16a55
Bunyamin-1058754 <1058754@hr.nl>
2025-04-28 01:19:11 +0200

    changed fonts

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div
     class="bg-white dark:bg-black text-black dark:text-white p-8 transition-colors duration-300 min-h-screen"
   >
+    <div class="font-sans"></div>
     <HomeView />
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+        sans: ['Inter', 'sans-serif',],
       },
     },
   },
