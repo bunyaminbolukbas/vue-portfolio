bb52e3bf9dd31570448de6c080339ea920493176
Bunyamin-1058754 <1058754@hr.nl>
2025-04-30 01:52:19 +0200

    updated tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,18 @@
+import animate from 'tailwindcss-animate'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif',],
+        sans: ['Inter', 'sans-serif'],
       },
     },
   },
   darkMode: 'class',
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 }
